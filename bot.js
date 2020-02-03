b0fc9b52a9e6d2c49e5643981b620f0dc4f160ce
James Pierce <james@empire-team.com>
2020-02-03 17:21:09 -0500

    pushing test split

--- bot.js
+++ bot.js
@@ -17,7 +17,8 @@ function roll(message, rollString){
     var totalRoll = 0;
     
     // cup represents the pool of dice being rolled, as in rolling a cup of Dice in Yahtzee
-    var cup = message.split("+");
+    var str = "How are you doing today?";
+var res = str.split(" ");
     /*for(var i = 0; i < cup.size; i++){
         var dice = cup[i].split("d");
         for(var j = 0; j < dice[0];j++){
