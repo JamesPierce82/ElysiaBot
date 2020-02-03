ac88755c998f1b0d55c7ecd585b571471eafe4c7
James Pierce <james@empire-team.com>
2020-02-03 17:29:14 -0500

    continue debugging roll funtion

--- bot.js
+++ bot.js
@@ -17,8 +17,8 @@ function roll(message, rollString){
     
     // cup represents the pool of dice being rolled, as in rolling a cup of Dice in Yahtzee
     var cup = message.content.split("+");
-    /*for(var i = 0; i < cup.size; i++){
-        var dice = cup[i].split("d");
+    for(var i = 0; i < cup.size; i++){
+        /*var dice = cup[i].split("d");
         for(var j = 0; j < dice[0];j++){
             tempRoll = Math.floor(Math.random() * dice[1]);
             if(j == 0){
@@ -26,9 +26,9 @@ function roll(message, rollString){
             } else {
                 output .= "+(" + tempRoll + ")";
             }
-            totalRoll += tempRoll;
+            totalRoll += tempRoll;*/
         }
-    }*/
+    }
     
     
     
