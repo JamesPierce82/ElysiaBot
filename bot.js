0d4e9e08cf9433d7fc0b7547c41ff6d4869e6b52
James Pierc <diegoatravesar@gmail.com>
2020-02-03 17:37:09 -0500

    output cuplength

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function roll(message, rollString){
     
     
     for(var i = 0; i < cup.length; i++){
-        var dice = cup[i].split("d");
+        //var dice = cup[i].split("d");
         /*for(var j = 0; j < dice[0];j++){
             tempRoll = Math.floor(Math.random() * dice[1]);
             if(j == 0){
@@ -35,7 +35,7 @@ function roll(message, rollString){
     
     
     
-    message.channel.send("You rolled : " + output + " which is equal to : " + totalRoll);
+    message.channel.send("You rolled : " + cup.length + " which is equal to : " + totalRoll);
     
 }
 
