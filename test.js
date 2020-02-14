7769269f7d73261daf0d8b4dd599d9b3053cc1ca
James Pierce <diegoatravesar@gmail.com>
2020-02-14 17:05:24 -0500

    added test.js
    * test.js is used to test the dialogflow code separated from the discord bot

---
+++ test.js
@@ -0,0 +1,23 @@
+var apiai = require('apiai');
+// Import all settings from the conf.js file
+let settings = require('./conf.js');
+
+// Declare variables from the conf.js file
+const token = settings.token;
+const apiToken = settings.apiToken;
+
+var app = apiai(apiToken);
+
+var request = app.textRequest('hello', {
+    sessionId: 'testID'
+});
+
+request.on('response', function(response){
+    console.log(response);
+});
+
+request.on('error', function(error){
+    console.log(error);
+});
+
+request.end();
